225ac23979533ff7df801eb7337b4ab1851c0a80
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2021-12-23 15:41:52 -0300

    Agregada lista de materias

--- .AutoGen_README's/AutoGen_README's.py
+++ .AutoGen_README's/AutoGen_README's.py
@@ -28,18 +28,22 @@ def generarTexto(materia : dict) -> str:
 
     # nombreSolo_TodasLasCarreras = f"{materia["nombre"]} - {materia["otrasCarreras"][carrera1]} - {materia["otrasCarreras"][carrera2]} - ... - {materia["otrasCarreras"][carreraN]}"
     nombreSolo_TodasLasCarreras = f"{materia['nombre']}"
-    for nombre in materia["otrasCarreras"]:
-        nombreSolo_TodasLasCarreras += f" - {nombre}"
 
     # Si no hay otras carreras
-    if len(materia["otrasCarreras"]) == 0: # len funciona para listas y para diccionarios
+    if "otrasCarreras" not in materia: # len funciona para listas y para diccionarios
         nombreCarrera_TodasLasCarreras = f"{materia['nombre']} de la carrera Ciencias de la Computación"
+    elif materia["otrasCarreras"] == None: # Está en otras carreras, pero con el mismo nombre
+        nombreCarrera_TodasLasCarreras = f"{materia['nombre']}"
     else: # Si hay otras carreras
         if type(materia["otrasCarreras"]) == list:
+            for nombre in materia["otrasCarreras"]:
+                nombreSolo_TodasLasCarreras += f" - {nombre}"
             nombreCarrera_TodasLasCarreras = f"{materia['nombre']}"
             for nombre in materia["otrasCarreras"]:
                 nombreCarrera_TodasLasCarreras += f" - {nombre}"
         elif type(materia["otrasCarreras"]) == dict:
+            for nombre in materia["otrasCarreras"].values():
+                nombreSolo_TodasLasCarreras += f" - {nombre}"
             nombreCarrera_TodasLasCarreras = f"{materia['nombre']} (Computación)"
             for carrera, nombre in materia["otrasCarreras"].items():
                 nombreCarrera_TodasLasCarreras += f" - {nombre} ({carrera})"
@@ -56,13 +60,73 @@ Exámenes viejos de la materia {nombreCarrera_TodasLasCarreras} de FaMAF.
 Cualquier contribución con un examen viejo que no esté es bienvenida, ya sea como pull request, o como mandar el examen por cualquier otro medio.
 {"(En esta materia todavía nadie subio ningún exámen, pero igual se puede contribuir)" if not hayExámenes else ""}
 
-{f"{materia['extra']}" if "extra" in materia else ""}
-        """
+{f"{materia['extra']}" if "extra" in materia else ""}\
+"""
     
     return texto
 
-print(generarTexto({"nombre":"Introducción a los algoritmos", "año":1, "cuatrimestre":1, "otrasCarreras":[]}))
-print("------------------------------")
-print(generarTexto({"nombre":"Álgebra", "año":1, "cuatrimestre":2, "otrasCarreras":["Álgebra 2", "Álgebra lineal"]}))
-print("------------------------------")
-print(generarTexto({"nombre":"Análisis matemático 2", "año":1, "cuatrimestre":2, "otrasCarreras":{"Matemática Aplicada": "Calculo 2"}}))
+
+
+materias = [
+    {
+        "nombre":"Análisis matemático 1",
+        "año":1, "cuatrimestre":1,
+        "otrasCarreras":{"Matemática aplicada": "Calculo 1"}
+    },
+    {
+        "nombre":"Introducción a los algoritmos",
+        "año":1, "cuatrimestre":1
+    },
+    {
+        "nombre":"Matemática discreta 1",
+        "año":1, "cuatrimestre":1,
+        "otrasCarreras":None # Matemática aplicada
+    },
+    {
+        "nombre":"Algoritmos y estructuras de datos 1",
+        "año":1, "cuatrimestre":2
+    },
+    {
+        "nombre":"Análisis matemático 2",
+        "año":1, "cuatrimestre":2,
+        "otrasCarreras":{"Matemática aplicada": "Calculo 2"}
+    },
+    {
+        "nombre":"Álgebra",
+        "año":1, "cuatrimestre":2,
+        "otrasCarreras":["Álgebra 2", "Álgebra lineal"]
+    },
+    {
+        "nombre":"Algoritmos y estructuras de datos 2",
+        "año":2, "cuatrimestre":1
+    },
+    {
+        "nombre":"Análisis numérico",
+        "año":2, "cuatrimestre":1,
+        "otrasCarreras":None # Matemática
+    },
+    {
+        "nombre":"Organización del computador",
+        "año":2, "cuatrimestre":1
+    },
+    {
+        "nombre":"Introducción a la lógica y a la computación",
+        "año":2, "cuatrimestre":2
+    },
+    {
+        "nombre":"Probabilidad y estadística",
+        "año":2, "cuatrimestre":2,
+        "otrasCarreras":{"profesorados": "Introducción a la probabilidad y estadística"}
+    },
+    {
+        "nombre":"Sistemas operativos",
+        "año":2, "cuatrimestre":2
+    }
+]
+
+
+
+
+for materia in materias:
+    print(generarTexto(materia))
+    print("------------------------------")
